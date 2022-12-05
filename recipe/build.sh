0c809308d5e9fa3c4f0b95b1d042118412c6ec4a
Mark Harfouche <mark.harfouche@gmail.com>
2022-12-04 22:22:25 -0500

    try adding compatibility flag

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-    make
-    # no make check :-(
-    make install PREFIX=${PREFIX}
+if [[ ${openssl} == "3" ]]; then
+    # https://github.com/AGWA/git-crypt/issues/232#issuecomment-1144996281
+    export CXXFLAGS='${CXXFLAGS} -DOPENSSL_API_COMPAT=0x30000000L'
+fi
+make
+# no make check :-(
+make install PREFIX=${PREFIX}
     
