ea3d3d460bb57013d89c2de34c73feef0493e0e8
Peter Williams <peter@newton.cx>
2022-12-07 03:51:03 +0000

    recipe/build.sh: fix quotes
    Need double quotes. Also `set -xeuo pipefail`.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -xeuo pipefail
+
 if [[ ${openssl} == "3" ]]; then
     # https://github.com/AGWA/git-crypt/issues/232#issuecomment-1144996281
-    export CXXFLAGS='${CXXFLAGS} -DOPENSSL_API_COMPAT=0x30000000L'
+    export CXXFLAGS="${CXXFLAGS} -DOPENSSL_API_COMPAT=0x30000000L"
 fi
+
 make
 # no make check :-(
 make install PREFIX=${PREFIX}
-    
